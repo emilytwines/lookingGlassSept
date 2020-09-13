4a7c4a3ee1f66d69f99c6a68f476e763d6368256
Adrian D. Cameron <adriandcameron@gmail.com>
2020-09-13 14:53:32 -0400

    Initial commit
    New office acticvity in process

--- Assets/MyStuff/Scripts/Printer.cs
+++ Assets/MyStuff/Scripts/Printer.cs
@@ -5,22 +5,20 @@ using System;
 public class Printer : MonoBehaviour
 
 {
-public void Awake()
-{
-   GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-
-}
-    private void OnTriggerEnter(Collider other)
-    {  
-
-   GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+    public void Awake()
+    {
+        GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
     }
-     
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
+    }
 
-   private void OnTriggerExit()
-   {
-   GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
-
-   }
+    private void OnTriggerExit()
+    {
+        GameObject.Find("printer").transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
+    }
 }
+
 
