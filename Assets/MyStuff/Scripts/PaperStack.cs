680e2a2bd207709cc04869ff4a8841cf2a7c5885
Adrian D. Cameron <adriandcameron@gmail.com>
2020-09-13 15:15:20 -0400

    Revert "Revert "Initial commit""
    This reverts commit c542b4259ff8ff5e880023e9769442f749f324ba.

---
+++ Assets/MyStuff/Scripts/PaperStack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperStack : InventoryItemBase
+{
+
+
+    public static PaperStack Instance { get; private set; }
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; } else { Debug.Log("Warning: multiple " + this + " in scene!"); }
+
+    }
+
+    public float scale = 1f;
+    public override string Name
+    {
+        get
+        {
+            return "PaperStack";
+        }
+    }
+
+
+    public override void OnUse()
+    {
+        base.OnUse();
+        this.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+}
+
