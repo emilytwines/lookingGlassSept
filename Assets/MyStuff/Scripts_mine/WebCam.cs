17d081966af236f9db5f897173085489b201c910
emilytwines <emilytwines@users.noreply.github.com>
2021-02-01 14:19:11 -0500

    webcam

---
+++ Assets/MyStuff/Scripts_mine/WebCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
