3dd4cb5f897e5533733038e44383b9d17a07e9da
emilytwines <emilytwines@users.noreply.github.com>
2021-02-23 11:37:18 -0500

    2/23
    to get on same page

---
+++ Assets/MyStuff/Scripts_mine/FinalManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class FinalManager : MonoBehaviour
+{
+    public GameObject player; 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player.GetComponent<ThirdPersonUserControl>().enabled = false; 
+        player.GetComponent<ThirdPersonCharacter>().enabled = false; 
+ 
+        player.GetComponent<Animator>().enabled = false;
+        player.GetComponent<Animation>().enabled = false;
+        player.GetComponent<CapsuleCollider>().enabled = false;
+
+        //BlueSuit: 
+        player.transform.GetChild(0).gameObject.SetActive(false);
+        
+        //followcamera:
+        player.transform.GetChild(4).gameObject.SetActive(true);
+        
+        //GameObject.Find("FollowCamera").SetActive(true); 
+
+
+    }
+
+    void Start()
+    {
+        GameObject.Find("CameraMain").SetActive(false); 
+        player.GetComponent<FirstPersonController>().enabled = true;
+        player.GetComponent<CharacterController>().enabled = true;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
